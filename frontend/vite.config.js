efde247a23654ec0124de5a5180bf0c4e6f3cea5
kookmao <noahsoskha1@gmail.com>
2025-06-10 01:21:15 +0300

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/eco-migration-app/',
+  base: '/eco-migration-app/', // <-- this must match your repo name
   plugins: [react()],
-});
+})
